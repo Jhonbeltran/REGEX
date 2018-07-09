358e00e8e97591e10871ae4cb05ca80c739264ee
Jhonbeltran <jbeltranleon@gmail.com>
2018-07-08 21:04:32 -0500

    Agrego ejemplo de partidos con 15 o más goles

--- scripts/python_results/regex_results.py
+++ scripts/python_results/regex_results.py
@@ -1,13 +1,25 @@
 import re
 
 #1877-03-03,England,Scotland,1,3,Friendly,London,England,FALSE
-pattern = re.compile(r'^([\d]{4,4})\-\d\d\-\d\d,(.*),Friendly,.*$')
+pattern = re.compile(r'^([\d]{4,4})\-\d\d\-\d\d,(.+),(.+),(\d+),(\d+),.*$')
 
 f = open('../../files/results.csv', 'r')
+
+more_that_ten_goals = 0
 
 for line in f:
 	res = re.match(pattern, line)
 	if(res):
-		print('* {}'.format(res.group(2)))
+		local = res.group(2)
+		visitor = res.group(3)
+		local_score = int(res.group(4))
+		visitor_score = int(res.group(5))
+		if (local_score + visitor_score >= 15):
+			print('* {}({}) - {}({})'.format(local, local_score,
+											 visitor, visitor_score))
+			more_that_ten_goals+=1
+			#break
+
+print(more_that_ten_goals)
 
 f.close()
