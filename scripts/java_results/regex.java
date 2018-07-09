506fb7f7a07aa0c595ffe806510263a6910f2b63
Jhonbeltran <jbeltranleon@gmail.com>
2018-07-08 23:51:58 -0500

    Uso libreria de regex en java

--- scripts/java_results/regex.java
+++ scripts/java_results/regex.java
@@ -2,17 +2,24 @@ import java.io.BufferedReader;
 import java.io.FileReader;
 import java.io.IOException;
 import java.io.InvalidObjectException;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 public class regex {
   public static void main(String[] args) {
     String file = "../../files/results.csv";
+    //En java se debe usar \\ en vez de \
+    Pattern pat = Pattern.compile("^2011\\-.*$");
 
     try {
       BufferedReader br = new BufferedReader(new FileReader(file));
       String line;
 
-      while((line = br.readLine()) != null) {
-        System.out.println(line);
+      while((line = br.readLine()) != null){
+        Matcher matcher = pat.matcher(line);
+        if(matcher.find()){
+          System.out.println(line);
+        }
       }
 
       br.close();
